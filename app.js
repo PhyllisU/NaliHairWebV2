e8164604162d91218206605b1be5c8f932487027
PhyllisU <phyllis_ugwonali@hotmail.fr>
2021-06-11 02:36:44 +0200

    maj package json2

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'nalihair_web_frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
